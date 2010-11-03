62f456a9203ab2ad65e28cf89d980d2ee36f459e
tily <tily05@gmail.com>
2010-11-03 13:39:59 +0900

    add example.

---
+++ example/your_friends.rb
@@ -0,0 +1,8 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib') unless $:.include?(File.dirname(__FILE__) + '/../lib')
+require 'rubygems'
+require 'faker/japanese'
+
+20.times {
+  name = Faker::Japanese::Name.name
+  puts "#{name} (#{name.yomi})"
+}
